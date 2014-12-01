ef50087e6c3132097729bf3a223732a2204aef2e
Jaybee <jbalog@village88.com>
2014-12-01 15:42:13 +0800

    initial commit - finished application

---
+++ application/controllers/random.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Random extends CI_Controller {
+
+	public function index()
+	{
+ 		// incremental counter
+ 		$counter = $this->session->userdata('count');
+ 		
+		($counter === FALSE) ? $counter = 1 : $counter++;
+
+		$this->session->set_userdata('count',$counter);
+		// $this->session->unset_userdata('count');
+ 		// random word
+ 		$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ 		$random_word = NULL;
+
+ 		for($i=0;$i<=14;$i++)
+ 		{
+ 			$random_word .= $characters[rand(0,strlen($characters)-1)];
+ 		}
+
+ 		$this->session->set_flashdata('random_word',$random_word);
+		
+		$this->load->view('random_view');
+	}
+
+}
+
+/* End of file random.php */
+/* Location: ./application/controllers/random.php */
---
+++ application/views/random_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" style="text/css" href="<?= base_url('assets/css/random.css') ?>">
+</head>
+<body>
+	<h2>Random Word (attempt # <?= $this->session->userdata('count'); ?>)</h2>
+	<h1><?= $this->session->flashdata('random_word');  ?></h1>
+	<a href="<?= base_url('random') ?>">Generate</a>
+
+</body>
+</html>	
